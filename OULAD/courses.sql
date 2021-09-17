09536054ebd062cd1ec9c8b4362e91cacb6557f9
Allie Wicklund <76880781+awicklund@users.noreply.github.com>
2021-09-17 19:32:29 -0400

    Added tables to SQL server

---
+++ OULAD/courses.sql
@@ -0,0 +1,27 @@
+CREATE TABLE courses(
+   code_module                VARCHAR(3) NOT NULL 
+  ,code_presentation          VARCHAR(5) NOT NULL
+  ,module_presentation_length INTEGER  NOT NULL
+);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('AAA','2013J',268);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('AAA','2014J',269);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('BBB','2013J',268);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('BBB','2014J',262);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('BBB','2013B',240);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('BBB','2014B',234);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('CCC','2014J',269);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('CCC','2014B',241);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('DDD','2013J',261);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('DDD','2014J',262);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('DDD','2013B',240);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('DDD','2014B',241);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('EEE','2013J',268);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('EEE','2014J',269);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('EEE','2014B',241);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('FFF','2013J',268);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('FFF','2014J',269);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('FFF','2013B',240);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('FFF','2014B',241);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('GGG','2013J',261);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('GGG','2014J',269);
+INSERT INTO courses(code_module,code_presentation,module_presentation_length) VALUES ('GGG','2014B',241);
